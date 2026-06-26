d6205bbf1ce178d838277fee0b734f497b93bfd8
WhimFreak <whimmyfreaky@gmail.com>
2026-06-26 09:44:13 +0800

    Tweaked scary maze layout, reset level when jumpscare triggered

--- sotera/src/scripts/scary_maze_lv_1.gd
+++ sotera/src/scripts/scary_maze_lv_1.gd
@@ -8,30 +8,34 @@ extends Node2D
 
 @onready var jumpscare: JumpScareController = $Jumpscare
 @onready var static_anim: AnimationPlayer = $StaticAnim
+@onready var player_starting_pos: Marker2D = $PlayerStartingPos
+@onready var timer: Timer = $Timer
+@onready var static_screen: TextureRect = $Static
 
-var jumpscare_triggered: bool = false
+var jumpscare_active: bool = false
 
-func _on_wall_area_body_entered(body: Node2D) -> void:
-	# Player takes damage from walls
-	hp -= 1
-	if hp == 0:
-		# Change to game over scene
-		get_tree().change_scene_to_file("res://assets/scenes/FortuneWheelScene.tscn")
+#func _on_wall_area_body_entered(body: Node2D) -> void:
+	## Player takes damage from walls
+	#hp -= 1
+	#if hp == 0:
+		## Change to game over scene
+		#get_tree().change_scene_to_file("res://assets/scenes/FortuneWheelScene.tscn")
 
 func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
-	if jumpscare_triggered:
+	if jumpscare_active:
 		return
 		
-	jumpscare_triggered = true
+	jumpscare_active = true
 	
 	# Disable player and exit area
 	player.visible = false
-	player.process_mode = Node.PROCESS_MODE_DISABLED
 	player.get_node("Footsteps").stop()
+	player.process_mode = Node.PROCESS_MODE_DISABLED
 	
 	# Disable darkness and trigger jumpscare
 	point_light.visible = false
 	darkness.visible = false
+	
 	jumpscare.start_jumpscare()
 	
 	# Wait till jumpscare is done
@@ -39,10 +43,19 @@ func _on_exit_area_body_entered(body: CharacterBody2D) -> void:
 	jumpscare.voice.stop()
 	
 	# Static
+	static_screen.global_position = player.global_position * static_screen.pivot_offset_ratio
 	static_anim.play("play_static")
 	await static_anim.animation_finished
 	
-	# Reset player state and go back to wheel
+	# Reset player state and level
 	player.visible = true
 	player.process_mode = Node.PROCESS_MODE_INHERIT
-	get_tree().call_deferred("change_scene_to_file", "res://assets/scenes/FortuneWheelScene.tscn")
+	player.global_position = player_starting_pos.global_position
+	
+	point_light.show()
+	darkness.show()
+	static_anim.play("RESET")
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	jumpscare_active = false
